836b462ad0404ebd235129c800a6b84ac76b8db4
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2022-03-26 09:57:45 +0100

    dodane przeszkadzajki

--- main.ts
+++ main.ts
@@ -67,6 +67,79 @@ function doGenerujpodloge (y: number) {
         `, SpriteKind.podloga)
     podloga2.y = y
 }
+function doGenerujPrzeszkadzajki (prz_y: number) {
+    przeszkadzajka = sprites.create(img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    przeszkadzajka,
+    [img`
+        . . . . e e e . . . . e e e 
+        . . . . c d d c . . c d d c 
+        . . . . c b d d f f d d b c 
+        . . . . c 3 b d b d d b 3 c 
+        . . . . f b 3 d d d d 3 b f 
+        . . . . e d d d d d d d d e 
+        b f b . e d f d d d d f d e 
+        f d f . f d d f d d f d d f 
+        f d f . f 2 d d b b d d b f 
+        f d f f b b 2 2 2 2 2 2 f . 
+        f b d b b d d d d d d b f . 
+        . f f f d d b d d d d d f . 
+        . . . f d f f d f f f d f . 
+        . . . f f . . f f . . f f . 
+        `,img`
+        . . . . . . . . . . . . . . 
+        . . . . e e e . . . . e e e 
+        . . . . c d d c . . c d d c 
+        . . . . c b d d f f d d b c 
+        . . . . c 3 b d b d d b 3 c 
+        . . . . f b 3 d d d d 3 b f 
+        . . . . e d d d d d d d d e 
+        . b f b e d f d d d d f d e 
+        . f d f f d d f d d f d d f 
+        . f d f b 2 d d b b d d b f 
+        . f b d b d 2 2 2 2 2 2 f . 
+        . . f f f d d d d d d d f . 
+        . . . . f d f f f d b d f . 
+        . . . . f f . . f f f f . . 
+        `,img`
+        . . . . . . . . . . . . . . 
+        . . . . e e e . . . . e e e 
+        . . . . c d d c . . c d d c 
+        . . . . c b d d f f d d b c 
+        . . . . c 3 b d b d d b 3 c 
+        . . . . f b 3 d d d d 3 b f 
+        . . . . e d d d d d d d d e 
+        b f b . e d f d d d d f d e 
+        f d f . f d d f d d f d d f 
+        f d f f b 2 d d b b d d b f 
+        f b d b b d 2 2 2 2 2 2 f . 
+        . f f f f d d d d d d d f . 
+        . . . . . f d f d b d f . . 
+        . . . . . f f f f f f . . . 
+        `],
+    200,
+    true
+    )
+    przeszkadzajka.setVelocity(randint(10, 20), 0)
+    przeszkadzajka.x = randint(10, scene.screenWidth() - 10)
+    przeszkadzajka.bottom = prz_y
+    przeszkadzajka.z = scene.screenWidth() * 1
+}
 function doGenerujdziure (predkosc: number, x: number, y: number, lewoprawo: number) {
     dziura2 = sprites.create(img`
         7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
@@ -79,9 +152,11 @@ function doGenerujdziure (predkosc: number, x: number, y: number, lewoprawo: num
     dziura2.z = lewoprawo
 }
 let dziura2: Sprite = null
+let przeszkadzajka: Sprite = null
 let podloga2: Sprite = null
 let czydziura = 0
 let ludzik: Sprite = null
+let level = 0
 ludzik = sprites.create(img`
     . . 4 4 4 . . . . 4 4 4 . . . . 
     . 4 5 5 5 e . . e 5 5 5 4 . . . 
@@ -157,6 +232,61 @@ for (let index = 0; index <= 6; index++) {
     doGenerujpodloge(index * 19)
     doGenerujdziure(randint(20, 30), -50, index * 19, randint(100, 200))
 }
+let poziomy = [
+[
+0,
+0,
+0,
+1,
+0,
+0
+],
+[
+0,
+1,
+0,
+1,
+0,
+0
+],
+[
+0,
+1,
+0,
+1,
+1,
+0
+],
+[
+1,
+1,
+0,
+1,
+1,
+0
+],
+[
+1,
+1,
+1,
+1,
+1,
+0
+],
+[
+1,
+1,
+1,
+1,
+1,
+1
+]
+]
+for (let index = 0; index <= 5; index++) {
+    if (poziomy[level][index] == 1) {
+        doGenerujPrzeszkadzajki((index + 1) * 19 - 2)
+    }
+}
 game.onUpdate(function () {
     for (let value2 of sprites.allOfKind(SpriteKind.dziura)) {
         if (value2.x <= value2.z * -1) {
@@ -176,6 +306,115 @@ game.onUpdate(function () {
             ludzik.ay = 180
         } else if (czydziura == 3) {
             game.over(false)
+        }
+    }
+    for (let value2 of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (value2.x <= value2.z * -1) {
+            value2.setVelocity(value2.vx * -1, 0)
+            animation.runImageAnimation(
+            przeszkadzajka,
+            [img`
+                . . . . e e e . . . . e e e 
+                . . . . c d d c . . c d d c 
+                . . . . c b d d f f d d b c 
+                . . . . c 3 b d b d d b 3 c 
+                . . . . f b 3 d d d d 3 b f 
+                . . . . e d d d d d d d d e 
+                b f b . e d f d d d d f d e 
+                f d f . f d d f d d f d d f 
+                f d f . f 2 d d b b d d b f 
+                f d f f b b 2 2 2 2 2 2 f . 
+                f b d b b d d d d d d b f . 
+                . f f f d d b d d d d d f . 
+                . . . f d f f d f f f d f . 
+                . . . f f . . f f . . f f . 
+                `,img`
+                . . . . . . . . . . . . . . 
+                . . . . e e e . . . . e e e 
+                . . . . c d d c . . c d d c 
+                . . . . c b d d f f d d b c 
+                . . . . c 3 b d b d d b 3 c 
+                . . . . f b 3 d d d d 3 b f 
+                . . . . e d d d d d d d d e 
+                . b f b e d f d d d d f d e 
+                . f d f f d d f d d f d d f 
+                . f d f b 2 d d b b d d b f 
+                . f b d b d 2 2 2 2 2 2 f . 
+                . . f f f d d d d d d d f . 
+                . . . . f d f f f d b d f . 
+                . . . . f f . . f f f f . . 
+                `,img`
+                . . . . . . . . . . . . . . 
+                . . . . e e e . . . . e e e 
+                . . . . c d d c . . c d d c 
+                . . . . c b d d f f d d b c 
+                . . . . c 3 b d b d d b 3 c 
+                . . . . f b 3 d d d d 3 b f 
+                . . . . e d d d d d d d d e 
+                b f b . e d f d d d d f d e 
+                f d f . f d d f d d f d d f 
+                f d f f b 2 d d b b d d b f 
+                f b d b b d 2 2 2 2 2 2 f . 
+                . f f f f d d d d d d d f . 
+                . . . . . f d f d b d f . . 
+                . . . . . f f f f f f . . . 
+                `],
+            200,
+            true
+            )
+        } else if (value2.x >= value2.z + scene.screenWidth()) {
+            value2.setVelocity(value2.vx * -1, 0)
+            animation.runImageAnimation(
+            przeszkadzajka,
+            [img`
+                e e e . . . . e e e . . . . 
+                c d d c . . c d d c . . . . 
+                c b d d f f d d b c . . . . 
+                c 3 b d d b d b 3 c . . . . 
+                f b 3 d d d d 3 b f . . . . 
+                e d d d d d d d d e . . . . 
+                e d f d d d d f d e . b f b 
+                f d d f d d f d d f . f d f 
+                f b d d b b d d 2 f . f d f 
+                . f 2 2 2 2 2 2 b b f f d f 
+                . f b d d d d d d b b d b f 
+                . f d d d d d b d d f f f . 
+                . f d f f f d f f d f . . . 
+                . f f . . f f . . f f . . . 
+                `,img`
+                . . . . . . . . . . . . . . 
+                e e e . . . . e e e . . . . 
+                c d d c . . c d d c . . . . 
+                c b d d f f d d b c . . . . 
+                c 3 b d d b d b 3 c . . . . 
+                f b 3 d d d d 3 b f . . . . 
+                e d d d d d d d d e . . . . 
+                e d f d d d d f d e b f b . 
+                f d d f d d f d d f f d f . 
+                f b d d b b d d 2 b f d f . 
+                . f 2 2 2 2 2 2 d b d b f . 
+                . f d d d d d d d f f f . . 
+                . f d b d f f f d f . . . . 
+                . . f f f f . . f f . . . . 
+                `,img`
+                . . . . . . . . . . . . . . 
+                e e e . . . . e e e . . . . 
+                c d d c . . c d d c . . . . 
+                c b d d f f d d b c . . . . 
+                c 3 b d d b d b 3 c . . . . 
+                f b 3 d d d d 3 b f . . . . 
+                e d d d d d d d d e . . . . 
+                e d f d d d d f d e . b f b 
+                f d d f d d f d d f . f d f 
+                f b d d b b d d 2 b f f d f 
+                . f 2 2 2 2 2 2 d b b d b f 
+                . f d d d d d d d f f f f . 
+                . . f d b d f d f . . . . . 
+                . . . f f f f f f . . . . . 
+                `],
+            200,
+            true
+            )
         }
     }
 })
